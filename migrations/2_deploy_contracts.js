3988bd387c5b901ce4d5e4251ebe9898e3d58276
Ben Lu <coopermilk123@gmail.com>
2018-08-23 15:19:30 -0700

    Add SAGA point contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var HotelCal = artifacts.require("./HotelCal.sol");
+var SAGApoint = artifacts.require("./SAGApoint");
 
 module.exports = function(deployer) {
   deployer.deploy(HotelCal, "hotelcal", "room");
+  deployer.deploy(SAGApoint);
 };
